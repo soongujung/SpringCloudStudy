f740e17d70e4d89d58b50ef920dcac2b69c21a0f
sgjung <playmymind@daum.net>
2020-12-02 08:26:03 +0900

    Spring In Action (ch14)  : 서비스 레지스트리 서버 (개발버전)

---
+++ eureka-local/src/main/java/io/study/eurekalocal/EurekaLocalApplication.java
@@ -0,0 +1,13 @@
+package io.study.eurekalocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaLocalApplication.class, args);
+	}
+
+}
