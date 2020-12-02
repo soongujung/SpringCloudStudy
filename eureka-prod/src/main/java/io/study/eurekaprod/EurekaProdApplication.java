f48b8f80a2f2f22644e1c955e87d5d59c45fa932
sgjung <playmymind@daum.net>
2020-12-03 08:07:55 +0900

    Spring In Action (ch14)  : 서비스 레지스트리 서버 (PROD 버전) 예제

---
+++ eureka-prod/src/main/java/io/study/eurekaprod/EurekaProdApplication.java
@@ -0,0 +1,13 @@
+package io.study.eurekaprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProdApplication.class, args);
+	}
+
+}
