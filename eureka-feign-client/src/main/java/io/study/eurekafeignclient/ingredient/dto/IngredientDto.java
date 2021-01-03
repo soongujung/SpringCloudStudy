f818d3e9626104d86c10de3246717d060a9069ac
sgjung <playmymind@daum.net>
2021-01-03 14:02:13 +0900

    REST 통신 기본구조 추가

---
+++ eureka-feign-client/src/main/java/io/study/eurekafeignclient/ingredient/dto/IngredientDto.java
@@ -0,0 +1,5 @@
+package io.study.eurekafeignclient.ingredient.dto;
+
+public class IngredientDto {
+
+}
---
+++ eureka-feign-client/src/main/java/io/study/eurekafeignclient/ingredient/web/IngredientController.java
@@ -0,0 +1,10 @@
+package io.study.eurekafeignclient.ingredient.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+}
