210ff9cccde18b2a14d7122ecdc40c375609fc26
sgjung <playmymind@daum.net>
2021-01-06 08:14:41 +0900

    Eureka Feign Client : Employee, Ingredient 관련로직추가

--- eureka-feign-client/src/main/java/io/study/eurekafeignclient/employee/EmployeeFeignClient.java
+++ eureka-feign-client/src/main/java/io/study/eurekafeignclient/employee/EmployeeFeignClient.java
@@ -1,12 +1,17 @@
 package io.study.eurekafeignclient.employee;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Component
 @FeignClient("ingredient-service")
-public interface EmployeeClient {
+public interface EmployeeFeignClient {
 
-	@GetMapping("/ingredient-service/{id}")
+	@GetMapping("/ingredient-service/employees")
+	EmployeeDto getEmployeeAll();
+
+	@GetMapping("/ingredient-service/employees/{id}")
 	EmployeeDto getEmployee (@PathVariable("id") Long id);
 }
