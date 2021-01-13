4e3f548656a3fc9af3e056d88bbc335a46a6fcb9
sgjung <playmymind@daum.net>
2021-01-14 08:07:53 +0900

    resilience4j 기반 feign client  : eureka 레지스트리, eureka 기반 서비스와 연동한 FeignClient  : 프로젝트 기본구조

---
+++ resilience4j-feign-client/src/test/java/io/study/resilience4jfeignclient/Resilience4jFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.resilience4jfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
